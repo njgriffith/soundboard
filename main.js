401346a60efe693e9f81e7d19297cd798cbcfb8c
njgriffith <njgriffith1@gmail.com>
2025-01-24 14:14:24 -0500

    made it better

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 function playSound(event){
     const audioPlayer = document.getElementById('audio');
     try{
-        audioPlayer.src = `/sounds/${event}`;
+        const soundName = event.parentNode.querySelector('label').innerText;
+        audioPlayer.src = `/sounds/${soundName}.mp3`;
         audioPlayer.play();
+        event.classList.toggle('selected');
     }
     catch (error){
         console.error(error);
     }
 }
+document.getElementById('audio').addEventListener('ended', (event) => {
+    document.querySelector('.selected').classList.toggle('selected');
+});
