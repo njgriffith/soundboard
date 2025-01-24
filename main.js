b36a8ea59604697274deecdd7c4eeb952a9d63af
Will Bucher <wjbucher3@gmail.com>
2025-01-23 21:40:57 -0500

    safari killed ogg sad

--- main.js
+++ main.js
@@ -1,26 +1,10 @@
-
-// FUCK YOU WE LOVE GLOBAL VARIABLES AND THE OGG FILE FORMAT!!!!!
-const sounds = {
-    1: {
-        "file": "bruh.ogg"
-    }, 
-    2: {
-        "file": "plug.ogg"
-    },
-    3: {
-        "file": "gay.ogg"
-    }
-}
-
 function playSound(event){
     const audioPlayer = document.getElementById('audio');
-    let soundPath = `/sounds/${sounds[event]["file"]}`;
     try{
-        audioPlayer.src = soundPath;
+        audioPlayer.src = `/sounds/${event}`;
         audioPlayer.play();
     }
     catch (error){
         console.error(error);
-        console.error(`"${soundPath}" was not found!`);
     }
 }
